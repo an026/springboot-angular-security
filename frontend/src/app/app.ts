10b6435ce247d500a137eb16b278f485ddfaa08f
mvu46 <allisonvu9@gmail.com>
2026-05-25 21:16:47 -0400

    Made starter frontend and backend.

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -5,8 +5,12 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('frontend');
+  greeting = {
+    id: 'XXX',
+    content: 'Hello World',
+  };
 }
